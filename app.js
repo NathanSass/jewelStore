0d850496fc6d0b906a67d24972821727220b5ad8
Nathan Sass <nsass711@gmail.com>
2014-06-03 09:01:14 -0700

    connected bootstrap, added a tab controller that toggles visibility

--- app.js
+++ app.js
@@ -11,6 +11,18 @@
 		this.ruby = ruby;
 	});
 
+	app.controller('TabController', function(){
+		this.tab = 1;
+
+		this.setTab = function(newValue){
+			this.tab = newValue
+		};
+
+		this.isSet = function(tabName){
+			return this.tab === tabName;
+		};
+	});
+
 	var gems = [
 
 	{
